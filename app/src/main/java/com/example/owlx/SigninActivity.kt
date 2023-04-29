2f9116d071035cced1eebceb2a8d8b000a27fbcd
luccamvmrey <luccamvmachadorey@gmail.com>
2023-04-29 00:31:36 -0300

    finished activity's front-end

--- app/src/main/java/com/example/owlx/SigninActivity.kt
+++ app/src/main/java/com/example/owlx/SigninActivity.kt
@@ -1,14 +1,14 @@
 package com.example.owlx
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
-import java.util.Calendar
+import java.util.*
 
 class SigninActivity : AppCompatActivity() {
 
     private var btnDateOfBirth: EditText? = null
-    private var cal = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +16,47 @@ class SigninActivity : AppCompatActivity() {
 
         btnDateOfBirth = findViewById(R.id.input_signin_date_of_birth)
 
-        btnDateOfBirth?.setOnClickListener {
+        //Prevents keyboard from being displayed.
+        btnDateOfBirth?.showSoftInputOnFocus = false
 
+        btnDateOfBirth?.setOnClickListener {
+            clickDatePicker()
         }
+
+        //TODO: User validation, same shit as before, just do it
     }
 
     private fun clickDatePicker() {
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
 
+        val dpd = DatePickerDialog(this,
+            { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+                val formattedDay: String = if (selectedDayOfMonth < 10) {
+                    "0$selectedDayOfMonth"
+                } else {
+                    "$selectedDayOfMonth"
+                }
+
+                val formattedMonth: String = if (selectedMonth < 10) {
+                    "0${selectedMonth + 1}"
+                } else {
+                    "${selectedMonth + 1}"
+                }
+
+                val selectedDate = "$formattedDay/$formattedMonth/$selectedYear"
+
+                btnDateOfBirth?.setText(selectedDate)
+            },
+            year,
+            month,
+            day
+        )
+
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
     }
 }
