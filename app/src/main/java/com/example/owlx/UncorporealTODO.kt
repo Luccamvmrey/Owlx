579681b3fbd390f8e5ed66c833af5c9b5073bfaf
luccamvmrey <luccamvmachadorey@gmail.com>
2023-05-09 17:50:37 -0300

    Fixed User Page, not yet finished

--- app/src/main/java/com/example/owlx/UncorporealTODO.kt
+++ app/src/main/java/com/example/owlx/UncorporealTODO.kt
@@ -3,5 +3,6 @@ package com.example.owlx
 //Create and Connect to Firestore - Done
 //Add user model - Done, WIP
 //TODO: Async Await code in AddProduct
+//TODO: Add localization to User Page
 //TODO: Change FindViewById for ViewBinding
 //TODO: Fix string names, but do it later
